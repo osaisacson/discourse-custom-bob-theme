53ed83f8bf7e2f81677bb2e34f0a2dd1fc43b9bd
Åsa Isacson <asaisacson@gmail.com>
2025-09-03 13:49:01 +0200

    Fix: Implement homepage via self-contained component

--- javascripts/discourse/initializers/custom-homepage-initializer.js
+++
@@ -1,9 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-// IMPORTANT: Use a relative path to import the component.
-// This is more reliable than the 'discourse/theme' path.
-import CustomHomepage from "../../common/components/custom-homepage";
-
-export default apiInitializer("0.8", (api) => {
-  api.renderInOutlet("custom-homepage", CustomHomepage);
-});
